2f37aa0fd5260cafb8c7a2dd759976f4651e01b8
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2024-12-31 14:18:12 +0300

    Minor touchups

--- game.js
+++ game.js
@@ -1,86 +1,87 @@
-var colors = ['red','green','blue','yellow','purple','cyan'];
+var colors = ["red", "green", "blue", "yellow", "purple", "cyan"];
 var sequenceColors = [];
 var playerColors = [];
 var gameStarted = false;
 var level = 0;
 
 // Keyboard key press event listener (game start)
-$(document).on('keypress', function(){
-    if (!gameStarted){
-        setTimeout(function(){
-            startGame();
-        },300)
-        gameStarted = true;
-    }
+$(document).on("keypress", function () {
+  if (!gameStarted) {
+    setTimeout(function () {
+      startGame();
+    }, 300);
+    gameStarted = true;
+  }
 });
 
 // Button click event listener (registering user clicks)
-$(".btn").on("click", function(){
-    var btnClicked = $(this).attr("id");
-    playerColors.push(btnClicked);
+$(".btn").on("click", function () {
+  var btnClicked = $(this).attr("id");
+  playerColors.push(btnClicked);
 
-    playSound(btnClicked);
+  playSound(btnClicked);
 
-    btnHighlight(btnClicked);
-    
-    compareColors(playerColors.length-1);
+  btnHighlight(btnClicked);
+
+  compareColors(playerColors.length - 1);
 });
 
 // Start the color sequence
-function startGame(){
-    playerColors = [];
+function startGame() {
+  playerColors = [];
 
-    level++;
-    $("#level-title").text("Level "+ level);
+  level++;
+  $("#level-title").text("Level " + level);
 
-    var randNum = Math.floor(Math.random()*6);
-    var randColor = colors[randNum];
-    sequenceColors.push(randColor);
+  var randNum = Math.floor(Math.random() * 6);
+  var randColor = colors[randNum];
+  sequenceColors.push(randColor);
 
-    $('#'+randColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randColor);
+  $("#" + randColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randColor);
 }
 
 // Comparing whether the player is pressing the correct buttons
-function compareColors(currentColourIndex){
-    if (playerColors[currentColourIndex] === sequenceColors[currentColourIndex]){
-        console.log('Correct!');
-        if (playerColors.length === sequenceColors.length){
-            setTimeout(function(){
-                startGame();
-            }, 1000)
-        } 
+function compareColors(currentColourIndex) {
+  if (playerColors[currentColourIndex] === sequenceColors[currentColourIndex]) {
+    if (playerColors.length === sequenceColors.length) {
+      setTimeout(function () {
+        startGame();
+      }, 1000);
     }
-    else {
-        console.log('False.');
+  } else {
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 150);
 
-        $("body").addClass("game-over");
-        setTimeout(function(){
-            $("body").removeClass("game-over");
-        }, 150);
+    var gameOverSound = new Audio("./sounds/wrong.mp3");
+    gameOverSound.play();
 
-        var gameOverSound = new Audio("./sounds/wrong.mp3");
-        gameOverSound.play();
+    $("#level-title").text(
+      "Game Over! Press Any Key To Restart!\nYour Score Is " + level
+    );
 
-        $("#level-title").text("Game Over! Press Any Key To Restart! Your Score Is " + level);
-
-        level = 0;
-        playerColors = [];
-        sequenceColors = [];
-        gameStarted = false;
-    }
+    level = 0;
+    playerColors = [];
+    sequenceColors = [];
+    gameStarted = false;
+  }
 }
 
 // Playing the sound of the button
-function playSound(colorName){
-    var sound = new Audio("./sounds/" + colorName + ".mp3");
-    sound.play();
+function playSound(colorName) {
+  var sound = new Audio("./sounds/" + colorName + ".mp3");
+  sound.play();
 }
 
 // Adding a highlight effect to the button
-function btnHighlight(colorName){
-    $("#" + colorName).addClass("pressed");
-    setTimeout(function(){
-        $("#" + colorName).removeClass("pressed");
-    },100)
+function btnHighlight(colorName) {
+  $("#" + colorName).addClass("pressed");
+  setTimeout(function () {
+    $("#" + colorName).removeClass("pressed");
+  }, 100);
 }
